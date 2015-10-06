0f0d0a071f51b9e5faadf96815fafc5b0721b3ee
Natan <natanseverolp@gmail.com>
2015-10-06 16:32:29 -0300

    Finalizando Menu Principal 2

--- js/menu_main.js
+++ js/menu_main.js
@@ -1,1 +1,12 @@
 // JQuery para efeito de Scroll do Menu Principal
+$(window).scroll(function(){
+	/* O ".scrollTop" retorna o valor do topo do rolamento no momento */
+	var top = $(window).scrollTop();
+	var nav = $('nav');
+
+	if(top >= 60) {
+		nav.addClass("menu-fixo");
+	} else {
+		nav.removeClass("menu-fixo");
+	}
+})
